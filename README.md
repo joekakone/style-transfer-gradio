c8947a275d6f86daa35452bcd5c05b57d2adddf9
joekakone <joseph.kakone@gmail.com>
2020-07-22 09:44:31 +0100

    Mise en ligne du code

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# style-transfer-gradio
+![Exemple](example.png)
+# Image Style Transfer
 Une implémentation simple du Image Style Transfer avec TFHub magenta/arbitrary-image-stylization-v1-256
+
+
+Le Style Transfer consiste à prendre deux image, une image de contenu et une image de style et les mélanger afin que la sortie ressemble à l'image de contenu mais peinte dans le style de l'image de style.
+
+
+Je me suis basé sur le tutoriel de la documentation officielle de Tensorflow, j'ai juste repris le code et je l'ai intégré à Gradio.
+Retrouvez la totalité du tutoriel ici [https://www.tensorflow.org/tutorials/generative/style_transfer](https://www.tensorflow.org/tutorials/generative/style_transfer)
+
+
+**Gradio** est un module qui vous permet de créer rapidement des composants d'interface utilisateur personnalisables autour de vos modèles TensorFlow ou PyTorch, ou même des fonctions Python arbitraires. Mélangez et associez les composants pour prendre en charge toute combinaison d'entrées et de sorties..... https://gradio.app/
+
+
+**Clôner le projet**
+
+`git clone https://github.com/joekakone/style-transfer-gradio.git`
+
+`cd style-transfer-gradio`
+
+ 
+**Mise en place de l'environnement virtuel**
+
+`virtualenv -p /usr/bin/python3 env`
+
+`source env/bin/activate`
+
+`pip install -r requirements.txt`
+
+
+**Lancer le projet**
+
+`python app.py`
---
+++ app.py
@@ -0,0 +1,70 @@
+# coding : utf-8
+
+'''
+Style Transfer
+Credits: https://www.tensorflow.org/tutorials/generative/style_transfer
+Gradio: https://gradio.app
+
+'''
+
+import os
+import numpy as np
+from PIL import Image
+import gradio as gr
+import tensorflow as tf
+import tensorflow_hub as hub
+
+
+FILE_PATH = 'temp.png'
+
+
+def preprocess_input(image):
+	max_dim = 512
+	image = Image.fromarray(image)
+	# save file temporarily
+	image.save(FILE_PATH)
+
+	img = tf.io.read_file(FILE_PATH)
+	# remove file
+	os.remove(FILE_PATH)
+	img = tf.image.decode_image(img, channels=3)
+	img = tf.image.convert_image_dtype(img, tf.float32)
+
+	shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+	long_dim = max(shape)
+	scale = max_dim / long_dim
+
+	new_shape = tf.cast(shape * scale, tf.int32)
+
+	img = tf.image.resize(img, new_shape)
+	img = img[tf.newaxis, :]
+
+	return img
+
+def style_transfer(input_content_image, input_style_image):
+	content_image = preprocess_input(input_content_image)
+	style_image = preprocess_input(input_style_image)
+
+	stylized_image = hub_module(tf.constant(content_image), tf.constant(style_image))[0]
+	stylized_image = np.array(stylized_image)
+	stylized_image = np.reshape(stylized_image, stylized_image.shape[1:])
+
+	return stylized_image
+
+
+# retreive model from tfhub
+hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+
+# inputs
+input1 = gr.inputs.Image(label='Content Image')
+input2 = gr.inputs.Image(label='Style Image')
+
+
+gr.Interface(
+    fn=style_transfer,
+    inputs=[input1, input2],
+    outputs='image',
+    title="Natural Style Transfer",
+	server_port=7878,
+	server_name='0.0.0.0'
+    ).launch()
